c0ef997de3c93dbf30c522e39e5eb200bd7270b7
Gregory Markus <gregmarkus1992@yahoo.com>
2023-10-28 13:27:28 -0500

    working on create option

--- pages/posts/new.js
+++ pages/posts/new.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import PostForm from '../../components/forms/PostForm';
-import { getUserById } from '../../api/userData';
+import { getUserIdFromUid } from '../../api/userData';
 import { useAuth } from '../../utils/context/authContext';
 
 export default function CreatePost() {
@@ -8,7 +8,7 @@ export default function CreatePost() {
   const [userId, setUserId] = useState(0);
 
   const getUserId = () => {
-    getUserById(user.uid)?.then(setUserId);
+    getUserIdFromUid(user.uid)?.then(setUserId);
   };
   useEffect(() => {
     getUserId();
