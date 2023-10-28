fae8f789520dce6ff06c3d352a0625862d0b04d3
Keana Cobarde <keanacobarde@gmail.com>
2023-10-28 13:45:52 -0500

    getting state to render the correct property type

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -23,14 +23,12 @@ export default function Posts() {
     getUserUid();
   }, [user.uid]);
 
-  console.warn(userId);
-
   return (
     <div>
       <div className="text-center my-4">
         <h1> POSTS </h1>
         <div className="d-flex flex-wrap">
-          {posts.map((post) => <PostCard postObj={post} key={post.id} onUpdate={getThePosts} userIdent={userId} />)}
+          {posts.map((post) => <PostCard postObj={post} key={post.id} onUpdate={getThePosts} userIdent={userId.id} />)}
         </div>
       </div>
     </div>
