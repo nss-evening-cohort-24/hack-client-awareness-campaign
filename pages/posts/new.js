7c16411f42212a8e65bb66ea00c76e57f61ff15f
Sean Bryant <smbryant1730@gmail.com>
2023-10-30 19:40:36 -0400

    fixed createPost function, and added assiociation functionality

--- pages/posts/new.js
+++ pages/posts/new.js
@@ -8,10 +8,20 @@ export default function CreatePost() {
   const [userId, setUserId] = useState(0);
 
   const getUserId = () => {
-    getUserIdFromUid(user.uid)?.then(setUserId);
+    getUserIdFromUid(user.uid)?.then((data) => {
+      // Check if data exists and has an 'id' property, then set userId
+      if (data && data.id) {
+        setUserId(data.id);
+      } else {
+        // If data doesn't contain 'id', set userId to 0
+        setUserId(0);
+      }
+    });
   };
+
   useEffect(() => {
     getUserId();
   }, [user]);
+
   return <PostForm userIdent={userId} />;
 }
