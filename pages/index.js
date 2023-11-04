263d6dc2a783b21dd3f302376e0a4c027c0d0533
Keana Cobarde <keanacobarde@gmail.com>
2023-11-04 00:39:12 -0500

    clering out linter errors

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,4 @@
-import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
-import { useAuth } from '../utils/context/authContext';
-
 function Home() {
-  const { user } = useAuth();
-
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -14,13 +8,7 @@ function Home() {
         maxWidth: '400px',
         margin: '0 auto',
       }}
-    >
-      <h1>Hello {user.displayName}! </h1>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
-    </div>
+    />
   );
 }
 
