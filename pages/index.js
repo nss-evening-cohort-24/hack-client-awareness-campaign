ac9b1bc0f16f710a6298a501cf6dcce3a63c7e3c
Keana Cobarde <keanacobarde@gmail.com>
2023-11-04 08:23:27 -0500

    set up folders, files, and hero banner section

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ function Home() {
       className="text-center d-flex flex-column justify-content-center align-content-center"
       style={{
         height: '90vh',
-        padding: '30px',
         maxWidth: '400px',
         margin: '0 auto',
       }}
