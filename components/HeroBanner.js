030a65740366b659232d27deaf3407146b1dc739
Keana Cobarde <keanacobarde@gmail.com>
2023-11-04 10:13:49 -0500

    saving for merge

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -1,23 +1,26 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// // function HeroBanner({
-// //   title, description, ctaText, ctaLink, backgroundImage,
-// // }) {
-// //   return (
-// //     <div className="hero-banner" style={{ backgroundImage: `url(${backgroundImage})` }}>
-// //       <div className="hb-text-area"></div>
-// //       <div className='hb-graphic-area'></div>
-// //     </div>
-// //   );
-// // }
+function HeroBanner({
+  title, description, ctaText, ctaLink, backgroundImage,
+}) {
+  return (
+    <div className="hero-banner" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="hero-content">
+        <h1 className="hero-title">{title}</h1>
+        <p className="hero-description">{description}</p>
+        <a href={ctaLink} className="cta-button">{ctaText}</a>
+      </div>
+    </div>
+  );
+}
 
-// HeroBanner.propTypes = {
-//   title: PropTypes.string.isRequired,
-//   description: PropTypes.string.isRequired,
-//   ctaText: PropTypes.string.isRequired,
-//   ctaLink: PropTypes.string.isRequired,
-//   backgroundImage: PropTypes.string.isRequired,
-// };
+HeroBanner.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  ctaText: PropTypes.string.isRequired,
+  ctaLink: PropTypes.string.isRequired,
+  backgroundImage: PropTypes.string.isRequired,
+};
 
-// export default HeroBanner;
+export default HeroBanner;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
-// import HeroBanner from '../components/HeroBanner';
+import HeroBanner from '../components/HeroBanner';
 
 function Home() {
   return (
-    <div />
+    <div className="hero-div">
+      <HeroBanner
+        title="Welcome"
+        description="Join us in the journey toward better mental health for all. At Awareness.org, we believe that mental well-being is a fundamental right, and we're dedicated to breaking down the barriers and stigmas that surround it.
+
+Our mission is to provide support, spread awareness, and create a community where individuals feel safe to share their stories, struggles, and triumphs. Together, we can make a difference by promoting understanding, empathy, and access to mental health resources.
+
+Explore our campaigns, resources, and stories from those who have faced the challenges of mental health head-on. Let's shatter the silence, stand together, and build a world where mental health is valued, understood, and supported.
+
+Join us on this journey. Your voice matters."
+      />
+    </div>
   );
 }
 
