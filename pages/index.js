587c92c2ac2370d5ef7a4ce75fb78fc68561a4df
Keana Cobarde <keanacobarde@gmail.com>
2023-11-04 10:24:17 -0500

    getting header somewhat done

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,15 @@ function Home() {
     <div className="hero-div">
       <HeroBanner
         title="Welcome"
-        description="Join us in the journey toward better mental health for all. At Awareness.org, we believe that mental well-being is a fundamental right, and we're dedicated to breaking down the barriers and stigmas that surround it.
+        description="Join us in the journey towards a more aware world.
 
-Our mission is to provide support, spread awareness, and create a community where individuals feel safe to share their stories, struggles, and triumphs. Together, we can make a difference by promoting understanding, empathy, and access to mental health resources.
+      Our mission is to provide support, spread awareness, and create a community where individuals feel safe to share their stories, struggles, and triumphs. Together, we can make a difference by promoting understanding and empathy.
 
-Explore our campaigns, resources, and stories from those who have faced the challenges of mental health head-on. Let's shatter the silence, stand together, and build a world where mental health is valued, understood, and supported.
+      Explore our campaigns, resources, and stories from those who have faced the challenges of mental health head-on. Let's shatter the silence and stand together.
 
-Join us on this journey. Your voice matters."
+      Join us on this journey. Your voice matters."
+        ctaText="Create a campaign"
+        ctaLink="http://localhost:3000/posts/new"
       />
     </div>
   );
